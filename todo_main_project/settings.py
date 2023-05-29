ed872400bd74f5366e8f8e34d131b60c62e8cd9c
dharmraj-magdum <dkmagdum23@gmail.com>
2023-05-30 01:06:22 +0530

    cors issue

--- todo_main_project/settings.py
+++ todo_main_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "corsheaders",
+    'corsheaders',
     "rest_framework",
     'rest_framework_simplejwt',
     'todo_api',
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -155,6 +155,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     os.getenv("CLIENT"),
 ]
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    os.getenv("CLIENT"),
+)
 # JWT Settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
